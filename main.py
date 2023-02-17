acc0bcaa86b23f6fa205243fddf9f5e695b6d82d
Jahirul islam <>
2023-02-17 09:58:08 +0000

    Added the flask project

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+# creating instance of class, __name__ is a convenient shortcut for this that is appropriate for most cases. This is needed so that Flask knows where to look for resources such as templates and static files.
+app = Flask(__name__)
+
+
+#route() decorator to tell Flask what URL should trigger our function
+@app.route("/")
+def hello_world():
+  return "HEllo world with jovian"
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
